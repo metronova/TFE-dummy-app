b219e1a41b06bd4539f1fe209abb332b7cecd1e5
metronova <83435645+metronova@users.noreply.github.com>
2021-06-03 06:14:45 +0800

    comment enhancement

--- app/src/main/java/com/example/tfedummyapp/MainActivity.java
+++ app/src/main/java/com/example/tfedummyapp/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page1);
+        setTitle("Real-Time Electrical Readings");
     }
 
     public void alarmPage(View v){
